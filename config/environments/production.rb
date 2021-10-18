f478169e4c02a19a5e2c3568bacd8e396f0d1f3f
Gabriel Pop <gabryel96@yahoo.com>
2021-10-18 13:31:20 +0300

    nothing important

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,5 +116,6 @@ Rails.application.configure do
   # these configuration options.
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
+  config.action_mailer.default_url_options = { host: 'http://personalchatrails.herokuapp.com/' }
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
